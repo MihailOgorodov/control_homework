97c8d63d92c831b47b3c9da87c8c1295bfd9512d
“Mihail <mic311089@gmail.com>
2023-02-12 20:21:58 +0400

    GetArray and PrintArray methods added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+string[] GetArray(string line)
+{
+          string[] lineArray = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+          string[] arr = new string[lineArray.Length];
+          for (int i = 0; i < lineArray.Length; i++)
+          {
+                    arr[i] = Convert.ToString(lineArray[i]);
+          }
+          return arr;
+}
+
+void PrintArray(string[] inArray2)
+{
+    for (int i = 0; i < inArray2.Length; i++)
+    {
+        Write($"{inArray2[i]} ");
+    }
+}
